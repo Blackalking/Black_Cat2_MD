f7d835e03b8634790d137e22c4f9a87fd9ab4087
ahmedvlack <153669023+ahmedvlack@users.noreply.github.com>
2024-06-26 20:39:04 +0300

    Update جروب قفل فتح.js

--- plugins/جروب قفل فتح.js
+++ plugins/جروب قفل فتح.js
@@ -1,20 +1,26 @@
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-  let isClose = { // Switch Case Like :v
-      'فتح': 'not_announcement',
-      'قفل': 'announcement',
-  }[(args[0] || '')]
-  if (isClose === undefined)
-      throw `
-*❒ أخــتر :*
-*❒ ${usedPrefix + command} قفل*
-*❒ ${usedPrefix + command} فتح*
+let isClose = { // Switch Case Like :v
+'open': 'not_announcement',
+'close': 'announcement',
+'فتح': 'not_announcement',
+'غلق': 'announcement',
+'on': 'not_announcement',
+'off': 'announcement',
+}[(args[0] || '')]
+if (isClose === undefined)
+throw `
+*[❗] !! الصيغة خاطئة*
+
+*┏━━━❲ ✨ مــثـال ✨ ❳━━━┓* 
+*┠┉↯ ${usedPrefix + command} فتح*
+*┠┉↯ ${usedPrefix + command} قفل*
 `.trim()
-  await conn.groupSettingUpdate(m.chat, isClose)
+await conn.groupSettingUpdate(m.chat, isClose)
+{m.reply('*[ ✔ ] تم تغير اعدادات الجروب بنجاح*')}
 }
-handler.help = ['group *open/close*']
+handler.help = ['group open / close', 'جروب فتح / غلق']
 handler.tags = ['group']
-handler.command = ['group', 'جروب'] 
+handler.command = /^(group|اعدادات|جروب)$/i
 handler.admin = true
 handler.botAdmin = true
-
 export default handler
